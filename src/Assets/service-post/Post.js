8d3bfc074ba31f63fc1965f89fbc81d767a8db8f
md. soliman <seo.mdsoliman@gmail.com>
2023-09-25 17:43:44 +0600

    calculator project add

--- src/Assets/service-post/Post.js
+++ src/Assets/service-post/Post.js
@@ -1,29 +1,21 @@
  const Post=[
     {
-        title: "To-Do List",
+        title: "To-Do List (REACT Project",
         path:"https://mdsoliman64.github.io/todo-list/"
     },
     {
-        title: "Quiz App (Grammer Guru)",
+        title: "Quiz App (MERN Project))",
         path:"https://grammarguru.onrender.com/"
     },
     {
-        title: "English Speaking Practice",
+        title: "English Speaking Practice (MERN project)",
         path:"https://learnenglish-dgff.onrender.com/"
     },
     {
-        title: "To-Do List",
-        path:"https://mdsoliman64.github.io/todo-list/"
+        title: "Calculator (React)",
+        path:"https://mdsoliman64.github.io/calculator/"
     },
-    {
-        title: "To-Do List",
-        path:"https://mdsoliman64.github.io/todo-list/"
-    },
-    {
-        title: "To-Do List",
-        path:"https://mdsoliman64.github.io/todo-list/"
-    },
-
+   
 ];
 
 export default Post;
